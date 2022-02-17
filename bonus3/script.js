1c6cd07a30ae35359ac7178407a0b6496d891912
andreablancovr@gmail.com <andreablancovr@gmail.com>
2022-02-17 07:54:42 +0100

    Corretto e completato anche bonus3 con un solo eventListener con even.target e i dataset. Mi ero svegliato presto per farlo ma ha funzionato quasi tutto al primo tentativo

--- bonus3/script.js
+++ bonus3/script.js
@@ -26,25 +26,31 @@ BUTTON.addEventListener('click',function() {
     // importo la difficoltà selezionata dall'utente
     const LVL_TEMP = difficulty.value;
 
-    function squareClick() {
-        console.log(this.innerHTML)
-        const TempElement = this
-        console.log( TempElement )
+    //creo funzione per da richiamare con l'eventListener sul GRID container
+    function squareClick(e) {
+        // console.log(this.innerHTML)
+        // const TempElement = this
+        const TempElement = e.target
+        // console.log( TempElement )
 
-        if( BOMB.includes(parseInt(TempElement.innerHTML) ) ) {
-            TempElement.classList.add('bomb'); //
+        // creo controllo per vedere la coincidenza tra n° dataset e n° bomba (nell'array BOMB) 
+        if( BOMB.includes(parseInt(TempElement.dataset.number) ) ) { //se conincide
+            TempElement.classList.add('bomb');
+            GRID.removeEventListener("click", squareClick);
+            // creo array (in realtà è un HTMLCollection) con tutte le caselle 
             const arraySquares = document.getElementsByClassName("square");
+            // creo ciclo per confrontare i numeri delle caselle nell'array con quelli presenti nell'array delle bombe
             for(let i = 0; i < arraySquares.length; i++) {
                 const square = arraySquares[i];
-                if( BOMB.includes( i + 1 ) ) {
+                // se i numeri coincidono, aggiungio classe bomb
+                if( BOMB.includes( i + 1) ) {
                     square.classList.add('bomb');
                 }
-                square.removeEventListener("click", squareClick);
+                // square.removeEventListener("click", squareClick);
             }
-
             alert (`Game Over!\nHai collezionato ${score} Punti!`)
         }
-        else {
+        else { // se non coincide
             TempElement.classList.add('selected');
             score++
             if( score === celleTot-16 ) {
@@ -52,7 +58,10 @@ BUTTON.addEventListener('click',function() {
             }
 
         }
-     }
+    }
+
+    GRID.addEventListener('click', squareClick);
+
 
     // creo variabile per il numero totale di celle in base alla difficoltà scelta
     if( LVL_TEMP === 'easy' ) {
@@ -69,7 +78,8 @@ BUTTON.addEventListener('click',function() {
     // creo celle, le formatto e le inserisco nel container
     for( let i = 1; i<celleTot+1; i++) {
         const SQUARE = document.createElement('div');
-        SQUARE.addEventListener('click', squareClick);
+        // SQUARE.addEventListener('click', squareClick);
+        SQUARE.dataset.number = i;
         SQUARE.classList.add('square');
         SQUARE.style.width = `calc( 100% / ${celleXRiga} )`;
         SQUARE.innerHTML = (i);
